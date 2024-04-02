49a99002a891dbcd4cdfe4244f30d7047eb4217e
UO275780 <uo275780@uniovi.es>
2024-04-02 14:48:15 +0200

    Lab09 - Clase - Sesion de Tipos empezada

--- src/ast/types/AbstractType.java
+++ src/ast/types/AbstractType.java
@@ -1,10 +1,74 @@
 package ast.types;
 
+import ast.ASTNode;
 import ast.AbstractASTNode;
+import ast.expressions.Expression;
+
+import java.util.List;
 
 public abstract class AbstractType extends AbstractASTNode implements Type {
 
     public AbstractType(int line, int column) {
         super(line, column);
+    }
+
+    @Override
+    public boolean isLogic(ASTNode node) {
+        return false;
+    }
+
+    @Override
+    public Type arithmetic(Type expressionType, ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type arithmetic(ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type compression(Type expressionType, ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type logical(Type expressionType, ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type logical(ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type parenthesis(List<Type> expressionList, ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type canBeCastTo(Type exprToCast, ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type brackets(Type expressionType, ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type dot(String structField, ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public Type mustPromotesTo(Type expression, ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public boolean isBuiltIn() {
+        return false;
     }
 }
--- src/ast/types/IntType.java
+++ src/ast/types/IntType.java
@@ -1,6 +1,10 @@
 package ast.types;
 
+import ast.ASTNode;
+import ast.expressions.Expression;
 import visitor.Visitor;
+
+import java.util.List;
 
 public class IntType extends AbstractType {
 
@@ -24,5 +28,68 @@ public class IntType extends AbstractType {
     @Override
     public <TP, TR> TR accept(Visitor<TP, TR> v, TP o) {
         return v.visit(this, o);
+    }
+
+    /*
+     *  COMPROBACIÓN DE TIPOS
+     */
+
+    @Override
+    public boolean isLogic(ASTNode node) {
+        return true;
+    }
+
+    @Override
+    public Type arithmetic(Type expressionType, ASTNode node) {
+        if( this.equals(expressionType) || expressionType instanceof ErrorType )
+            return expressionType;
+        else
+            return super.arithmetic(expressionType, node);
+    }
+
+    @Override
+    public Type arithmetic(ASTNode node) {
+        return this;
+    }
+
+    @Override
+    public Type compression(Type expressionType, ASTNode node) {
+        if( this.equals(expressionType) || expressionType instanceof ErrorType )
+            return expressionType;
+        else
+            return super.compression(expressionType, node);
+    }
+
+    @Override
+    public Type logical(Type expressionType, ASTNode node) {
+        if( this.equals(expressionType) || expressionType instanceof ErrorType )
+            return expressionType;
+        else
+            return super.logical(expressionType, node);
+    }
+
+    @Override
+    public Type logical(ASTNode node) {
+        return this;
+    }
+
+    @Override
+    public Type canBeCastTo(Type exprToCast, ASTNode node) {
+        if( exprToCast.isBuiltIn() || exprToCast instanceof ErrorType)
+            return exprToCast;
+        return super.canBeCastTo(exprToCast, node);
+    }
+
+    @Override
+    public Type mustPromotesTo(Type expression, ASTNode node) {
+        if( this.equals(expression) || expression instanceof ErrorType )
+            return expression;
+        else
+            return super.mustPromotesTo(expression, node);
+    }
+
+    @Override
+    public boolean isBuiltIn() {
+        return true;
     }
 }
